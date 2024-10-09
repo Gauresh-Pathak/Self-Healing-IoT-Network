31b2a1041abc1eb704577d737ecf9eeadde49815
Gauresh Pathak <108048576+Gauresh-Pathak@users.noreply.github.com>
2024-10-09 10:00:00 +0530

    added node detection and failover logic

--- node.cpp
+++ node.cpp
@@ -2,26 +2,23 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
-// network config
 const char* ssid = "YOUR_WIFI_NAME";
 const char* password = "YOUR_WIFI_PASSWORD";
 
-// each node has a unique id
-int nodeID = 1; // change this to 2, 3 for other nodes
+int nodeID = 1;
 
-// all node IPs on the network
 String nodeIPs[] = {
-  "192.168.1.101", // node 1
-  "192.168.1.102", // node 2
-  "192.168.1.103"  // node 3
+  "192.168.1.101",
+  "192.168.1.102",
+  "192.168.1.103"
 };
 int totalNodes = 3;
 
-// heartbeat timing
 unsigned long lastHeartbeat = 0;
-int heartbeatInterval = 5000; // every 5 seconds
+int heartbeatInterval = 5000;
+unsigned long lastSeen[3] = {0, 0, 0};
+int deadThreshold = 15000; // node considered dead after 15 seconds
 
-// node status
 bool nodeStatus[3] = {true, true, true};
 
 WiFiServer server(80);
@@ -31,8 +28,32 @@ void sendHeartbeat(String targetIP) {
   HTTPClient http;
   String url = "http://" + targetIP + "/heartbeat?from=" + String(nodeID);
   http.begin(client, url);
-  http.GET();
+  int httpCode = http.GET();
+  
+  // if node didnt respond mark it as dead
+  if (httpCode != 200) {
+    int nodeIndex = 0;
+    for (int i = 0; i < totalNodes; i++) {
+      if (nodeIPs[i] == targetIP) nodeIndex = i;
+    }
+    nodeStatus[nodeIndex] = false;
+    Serial.println("Node " + String(nodeIndex + 1) + " not responding!");
+  }
   http.end();
+}
+
+void checkDeadNodes() {
+  for (int i = 0; i < totalNodes; i++) {
+    if (i != nodeID - 1) {
+      if (millis() - lastSeen[i] > deadThreshold) {
+        if (nodeStatus[i]) {
+          nodeStatus[i] = false;
+          Serial.println("NODE " + String(i + 1) + " IS DOWN!");
+          Serial.println("Taking over responsibilities of Node " + String(i + 1));
+        }
+      }
+    }
+  }
 }
 
 void handleClient(WiFiClient client) {
@@ -40,9 +61,9 @@ void handleClient(WiFiClient client) {
   client.flush();
 
   if (request.indexOf("/heartbeat") != -1) {
-    // another node is alive
     int fromNode = request.substring(request.indexOf("from=") + 5).toInt();
     nodeStatus[fromNode - 1] = true;
+    lastSeen[fromNode - 1] = millis();
     Serial.println("Heartbeat received from Node " + String(fromNode));
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/plain");
@@ -51,7 +72,6 @@ void handleClient(WiFiClient client) {
   }
 
   if (request.indexOf("/status") != -1) {
-    // return status of all nodes
     String response = "";
     for (int i = 0; i < totalNodes; i++) {
       response += "Node " + String(i + 1) + ": " + (nodeStatus[i] ? "ALIVE" : "DEAD") + "\n";
@@ -75,26 +95,28 @@ void setup() {
   Serial.println(" Connected!");
   Serial.println("Node " + String(nodeID) + " IP: " + WiFi.localIP().toString());
 
+  lastSeen[nodeID - 1] = millis();
   server.begin();
-  Serial.println("Node " + String(nodeID) + " started and listening...");
+  Serial.println("Node " + String(nodeID) + " online and monitoring network...");
 }
 
 void loop() {
-  // handle incoming requests from other nodes
   WiFiClient client = server.available();
   if (client) {
     handleClient(client);
   }
 
-  // send heartbeat to all other nodes every 5 seconds
   if (millis() - lastHeartbeat >= heartbeatInterval) {
     lastHeartbeat = millis();
 
     for (int i = 0; i < totalNodes; i++) {
       if (i != nodeID - 1) {
-        Serial.println("Sending heartbeat to Node " + String(i + 1));
+        Serial.println("Pinging Node " + String(i + 1) + "...");
         sendHeartbeat(nodeIPs[i]);
       }
     }
+
+    // check if any node has gone silent
+    checkDeadNodes();
   }
 }
